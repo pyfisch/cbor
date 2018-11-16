7bda9c938e6980d2f36d23c66bd412299a87d984
chrysn <chrysn@fsfe.org>
2018-11-16 13:26:32 +0100

    de/Read: Move scratch buffers to readers
    This simplifies the parse_indefinite_* functions as they can now rely on
    the full string to be in the reader's scratch buffer.
    
    It adds a bit of interface to the Reader as the deserializer can now
    specifically ask for reads to go directly to the scratch buffer or
    (basically as before) to "simply read" (to scratch or a reference
    return), plus the new function for clearing and accessing the buffer --
    but then again the functions themselves are more straightforward (no
    passing of a buf that may or may not be ignored by implementations).
    
    The convert_str method was changed into an associated function; the
    offset position is extracted from self and manually passed in to
    convert_str. This is not strictly necessary at this point yet (given
    that it's currenlty fed view_buffer results), but will be later.

--- src/de.rs
+++ src/de.rs
@@ -79,7 +79,6 @@ where
 /// A Serde `Deserialize`r of CBOR data.
 pub struct Deserializer<R> {
     read: R,
-    buf: Vec<u8>,
     remaining_depth: u8,
 }
 
@@ -112,7 +111,6 @@ where
     pub fn new(read: R) -> Self {
         Deserializer {
             read,
-            buf: Vec::new(),
             remaining_depth: 128,
         }
     }
@@ -184,16 +182,14 @@ where
     where
         V: de::Visitor<'de>,
     {
-        self.buf.clear();
-        match self.read.read(len, &mut self.buf, 0)? {
+        match self.read.read(len)? {
             Reference::Borrowed(buf) => visitor.visit_borrowed_bytes(buf),
-            Reference::Copied => visitor.visit_bytes(&self.buf),
+            Reference::Copied => visitor.visit_bytes(self.read.view_buffer()),
         }
     }
 
     fn parse_indefinite_bytes(&mut self) -> Result<&[u8]> {
-        let mut offset = 0;
-        self.buf.clear();
+        self.read.clear_buffer();
         loop {
             let byte = self.parse_u8()?;
             let len = match byte {
@@ -212,29 +208,18 @@ where
                 _ => return Err(self.error(ErrorCode::UnexpectedCode)),
             };
 
-            match self.read.read(len, &mut self.buf, offset)? {
-                Reference::Borrowed(buf) => {
-                    let new_len = offset + len;
-                    if new_len > self.buf.len() {
-                        self.buf.resize(new_len, 0);
-                    }
-                    self.buf[offset..].copy_from_slice(buf);
-                }
-                Reference::Copied => {}
-            }
-
-            offset += len;
+            self.read.read_to_buffer(len)?;
         }
 
-        Ok(&self.buf[..offset])
+        Ok(self.read.view_buffer())
     }
 
-    fn convert_str<'a>(&self, buf: &'a [u8]) -> Result<&'a str> {
+    fn convert_str<'a>(buf: &'a [u8], buf_end_offset: u64) -> Result<&'a str> {
         match str::from_utf8(buf) {
             Ok(s) => Ok(s),
             Err(e) => {
                 let shift = buf.len() - e.valid_up_to();
-                let offset = self.read.offset() - shift as u64;
+                let offset = buf_end_offset - shift as u64;
                 Err(Error::syntax(ErrorCode::InvalidUtf8, offset))
             }
         }
@@ -244,22 +229,21 @@ where
     where
         V: de::Visitor<'de>,
     {
-        self.buf.clear();
-        match self.read.read(len, &mut self.buf, 0)? {
+        match self.read.read(len)? {
             Reference::Borrowed(buf) => {
-                let s = self.convert_str(buf)?;
+                let s = Self::convert_str(buf, self.read.offset())?;
                 visitor.visit_borrowed_str(s)
             }
             Reference::Copied => {
-                let s = self.convert_str(&self.buf)?;
+                let offset = self.read.offset();
+                let s = Self::convert_str(self.read.view_buffer(), offset)?;
                 visitor.visit_str(s)
             }
         }
     }
 
     fn parse_indefinite_str(&mut self) -> Result<&str> {
-        let mut offset = 0;
-        self.buf.clear();
+        self.read.clear_buffer();
         loop {
             let byte = self.parse_u8()?;
             let len = match byte {
@@ -278,21 +262,11 @@ where
                 _ => return Err(self.error(ErrorCode::UnexpectedCode)),
             };
 
-            match self.read.read(len, &mut self.buf, offset)? {
-                Reference::Borrowed(buf) => {
-                    let new_len = offset + len;
-                    if new_len > self.buf.len() {
-                        self.buf.resize(new_len, 0);
-                    }
-                    self.buf[offset..].copy_from_slice(buf);
-                }
-                Reference::Copied => {}
-            }
-
-            offset += len;
+            self.read.read_to_buffer(len)?;
         }
 
-        self.convert_str(&self.buf[..offset])
+        let offset = self.read.offset();
+        Self::convert_str(self.read.view_buffer(), offset)
     }
 
     fn recursion_checked<F, T>(&mut self, f: F) -> Result<T>
