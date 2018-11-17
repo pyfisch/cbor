0d2cbc91bce2a5d7419706a268729d9b060afb3d
chrysn <chrysn@fsfe.org>
2018-11-17 12:55:41 +0100

    Add an experimental mutable-slice reader
    This is usable as-is, but for a practically mergable implementation I'd
    prefer to have the unsafe part provided by a data structure crate.

--- src/read.rs
+++ src/read.rs
@@ -302,3 +302,132 @@ impl<'a> Read<'a> for SliceRead<'a> {
         self.index as u64
     }
 }
+
+/// A CBOR input source that reads from a slice of bytes, and can move data around internally to
+/// reassemble indefinite strings without the need of an allocated scratch buffer.
+///
+/// This is implemented using unsafe code, which relies on the implementation not to mutate the
+/// slice wherever immutable references have been handed out; that position is tracked in
+/// buffer_end.
+pub struct MutSliceRead<'a> {
+    /// A complete view of the reader's data. It is promised that bytes before buffer_end are not
+    /// mutated any more.
+    slice: &'a mut [u8],
+    /// Read cursor position in slice
+    index: usize,
+    /// Index when clear() was last called
+    buffer_start: usize,
+    /// End of the buffer area that contains all bytes read_into_buffer. Doubles as end of
+    /// immutability guarantee.
+    buffer_end: usize,
+}
+
+impl<'a> MutSliceRead<'a> {
+    /// Creates a CBOR input source to read from a slice of bytes.
+    pub fn new(slice: &'a mut [u8]) -> MutSliceRead<'a> {
+        MutSliceRead {
+            slice,
+            index: 0,
+            buffer_start: 0,
+            buffer_end: 0,
+        }
+    }
+
+    fn end(&self, n: usize) -> Result<usize> {
+        match self.index.checked_add(n) {
+            Some(end) if end <= self.slice.len() => Ok(end),
+            _ => {
+                Err(Error::syntax(
+                    ErrorCode::EofWhileParsingValue,
+                    self.slice.len() as u64,
+                ))
+            }
+        }
+    }
+}
+
+impl<'a> private::Sealed for MutSliceRead<'a> {}
+
+impl<'a> Read<'a> for MutSliceRead<'a> {
+    #[inline]
+    fn next(&mut self) -> io::Result<Option<u8>> {
+        // This is duplicated from SliceRead, can that be eased?
+        Ok(if self.index < self.slice.len() {
+            let ch = self.slice[self.index];
+            self.index += 1;
+            Some(ch)
+        } else {
+            None
+        })
+    }
+
+    #[inline]
+    fn peek(&mut self) -> io::Result<Option<u8>> {
+        // This is duplicated from SliceRead, can that be eased?
+        Ok(if self.index < self.slice.len() {
+            Some(self.slice[self.index])
+        } else {
+            None
+        })
+    }
+
+    fn clear_buffer<'b>(&'b mut self) {
+        self.buffer_start = self.index;
+        self.buffer_end = self.index;
+    }
+
+    fn read_to_buffer(&mut self, n: usize) -> Result<()> {
+        let end = self.end(n)?;
+        assert!(self.buffer_end <= self.index, "MutSliceRead invariant violated: scratch buffer exceeds index");
+        self.slice[self.buffer_end..end].rotate_left(self.index - self.buffer_end);
+        self.buffer_end += n;
+        self.index = end;
+
+        Ok(())
+    }
+
+    #[inline]
+    fn read(&mut self, n: usize) -> Result<Reference<'a>> {
+        let end = self.end(n)?;
+        let slice = &self.slice[self.index..end];
+        self.index = end;
+
+        // Not technically required to keep track of things under realistic (ie. either read
+        // or clear_buffer+n*read_to_buffer is called) conditions, but given we don't want to rely
+        // on these condition to maintain safety, this updates the immutability contract of the
+        // slice.
+        self.buffer_start = self.index;
+        self.buffer_end = self.index;
+
+        // Unsafe: Extending the lifetime from during-the-function to 'a ("for as long as
+        // MutSliceRead is in mutable control of the data"), which is OK because MutSliceRead
+        // promises to never mutate data before buffer_end.
+        let extended_result = unsafe { &*(slice as *const _) };
+
+        Ok(Reference::Borrowed(extended_result))
+    }
+
+    fn view_buffer<'b>(&'b self) -> &'b [u8] {
+        // No unsafe tricks necessary here -- we could give out a longer lifetime, because to us
+        // all data in the buffer is immutable, but the Vec<u8> based readers can't do that.
+        &self.slice[self.buffer_start..self.buffer_end]
+    }
+
+    #[inline]
+    fn read_into(&mut self, buf: &mut [u8]) -> Result<()> {
+        // This is duplicated from SliceRead, can that be eased?
+        let end = self.end(buf.len())?;
+        buf.copy_from_slice(&self.slice[self.index..end]);
+        self.index = end;
+        Ok(())
+    }
+
+    #[inline]
+    fn discard(&mut self) {
+        self.index += 1;
+    }
+
+    fn offset(&self) -> u64 {
+        self.index as u64
+    }
+}
