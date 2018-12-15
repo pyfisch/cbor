6248deefe5827a368331557f20da871b8a4b14fa
Oliver Goodman <oag@optusnet.com.au>
2018-12-15 16:30:07 +1100

    Adds self_describe to SerializerOptions. Adds SerializerOptions::to_vec. Removes to_vec_with_options_sd.

--- tests/ser.rs
+++ tests/ser.rs
@@ -135,6 +135,10 @@ fn test_self_describing() {
         serializer.serialize_u64(9).unwrap();
     }
     assert_eq!(vec, b"\xd9\xd9\xf7\x09");
+
+    let sd = ser::SerializerOptions{ self_describe: true, ..Default::default() };
+    let vec = sd.to_vec(&9).unwrap();
+    assert_eq!(vec, b"\xd9\xd9\xf7\x09");
 }
 
 #[test]
