fbed55314550a20f73aed04e2e319ccbbc6ef382
Windel Bouwman <windel@windel.nl>
2020-01-06 21:59:54 +0100

    Only perform test in cfg std.

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -1,27 +1,31 @@
 #[macro_use]
 extern crate serde_derive;
 
-use serde_cbor;
+#[cfg(feature = "std")]
+mod roundtrip {
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct MyStuff {
-    #[serde(flatten)]
-    data: MyStuffType,
-}
+    use serde_cbor;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-enum MyStuffType {
-    Ver1 { x: f64, y: f32 },
-    Ver2,
-}
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    struct MyStuff {
+        #[serde(flatten)]
+        data: MyStuffType,
+    }
 
-#[test]
-/// Test roundtrip operation on a serde data structure.
-fn test_roundtrip() {
-    let stuff1 = MyStuff {
-        data: MyStuffType::Ver1 { x: 2.5, y: 2.5 },
-    };
-    let data_bytes = serde_cbor::to_vec(&stuff1).unwrap();
-    let stuff2 = serde_cbor::from_slice(&data_bytes).unwrap();
-    assert_eq!(stuff1, stuff2);
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    enum MyStuffType {
+        Ver1 { x: f64, y: f32 },
+        Ver2,
+    }
+
+    #[test]
+    /// Test roundtrip operation on a serde data structure.
+    fn test_roundtrip() {
+        let stuff1 = MyStuff {
+            data: MyStuffType::Ver1 { x: 2.5, y: 2.5 },
+        };
+        let data_bytes = serde_cbor::to_vec(&stuff1).unwrap();
+        let stuff2 = serde_cbor::from_slice(&data_bytes).unwrap();
+        assert_eq!(stuff1, stuff2);
+    }
 }
